a18bc069a12ca1a43f9144998313449bc50c015b
RiRi <hyerin77@naver.com>
2022-11-28 14:05:16 +0900

    인터페이스 기반, 클래스 기반 프록시 테스트

---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -0,0 +1,13 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+public class ConcreteClient {
+    private ConcreteLogic concreteLogic; // ConcreteLogic, TimeProxy 모두 주입 가능.
+
+    public ConcreteClient(ConcreteLogic concreteLogic){
+        this.concreteLogic = concreteLogic;
+    }
+
+    public void execute(){
+        concreteLogic.operation();
+    }
+}
