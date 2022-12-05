8580698ef29f488cb30e00efbe6af796ad672f52
RiRi <hyerin77@naver.com>
2022-12-05 09:17:11 +0900

    리플렉션 Test - 클래스, 메서드 메타정보를 사용하여 동적으로 호출하는 메서드를 변경할 수 있다.
    - 컴파일 시점에 오류를 잡을 수 없다.
    -> 리플렉션을 일반적으로 사용하면 안된다.
    - 프레임워크 개발이나 매우 일반적인 공통처리가 필요할 때 부분적으로 주의해서 사용해야 한다.

---
+++ src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,56 @@
+package hello.proxy.jdkdynamic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ReflectionTest {
+
+    @Test
+    void reflection0(){
+        Hello target = new Hello();
+
+        // 공통 로직1 시작
+        log.info("start");
+        String result1 = target.callA();
+        log.info("result={}", result1);
+
+        // 공통 로직1 시작
+        log.info("start");
+        String result2 = target.callB();
+        log.info("result={}", result2);
+    }
+
+    @Test
+    void reflection1() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // 클래스 정보
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        // callA 메서드 정보
+        Method methodCallA = classHello.getMethod("callA");
+        Object result1 = methodCallA.invoke(target); // target의 callA 메서드를 호출.
+        log.info("result1={}", result1);
+
+        // callB 메서드 정보
+        Method methodCallB = classHello.getMethod("callB");
+        Object result2 = methodCallB.invoke(target); // target의 callA 메서드를 호출.
+        log.info("result2={}", result2);
+    }
+
+    @Slf4j
+    static class Hello{
+        public String callA(){
+            log.info("callA");
+            return "A";
+        }
+
+        public String callB(){
+            log.info("callB");
+            return "B";
+        }
+    }
+}
